d9654f928efa9bab52da37b95c3cc3040d226dd2
ming <44185091+selene4515@users.noreply.github.com>
2022-11-17 13:55:53 +0900

    카드리스트 error 해결
    키 값 추가

--- src/components/card/CardList.jsx
+++ src/components/card/CardList.jsx
@@ -2,8 +2,8 @@ import GaebalCard from "./GaebalCard";
 
 const CardList = (props) => {
   const cardGaebalD = props.cardGaebalData;
-  const cardGaebalList = cardGaebalD.map((cardText) => (
-    <GaebalCard cardText={cardText} />
+  const cardGaebalList = cardGaebalD.map((cardText, index) => (
+    <GaebalCard cardText={cardText} key={index} />
   ));
   return (
     <div className="List_List_container__JnQMS" style={{ textAlign: "center" }}>
