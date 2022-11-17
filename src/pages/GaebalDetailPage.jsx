2ac4bddc8a8917e2b539e1a4028ee0097acc9138
ming <44185091+selene4515@users.noreply.github.com>
2022-11-17 14:39:19 +0900

    GabelDetailPage 아코디언 구현
    하단 회색 div

--- src/pages/GaebalDetailPage.jsx
+++ src/pages/GaebalDetailPage.jsx
@@ -17,6 +17,8 @@ const GaebalDetailPage = (props) => {
 
   const [isHovering, setIsHovering] = useState(false);
 
+  const [acod, setAcod] = useState(false);
+
   return (
     <div>
       <Header />
@@ -578,12 +580,40 @@ const GaebalDetailPage = (props) => {
                         <br className="break-lg" />
                         용도로 사용할 수 없습니다.
                       </h5>
-                      <button type="button" className="btn-open">
+                      <button
+                        type="button"
+                        className={acod ? "btn-open" : ""}
+                        onClick={() => {
+                          acod ? setAcod(false) : setAcod(true);
+                        }}
+                      >
                         <i className="icon-arrow_down"></i>
                       </button>
                     </div>
                   </div>
+                  {acod && (
+                    <div class="WarningBody_WarningBody__Ha5lJ">
+                      <p class="WarningBody_WarningBody_p__a_vz5">
+                        본 채용 정보는&nbsp;<strong>블루비커</strong>에서 제공한
+                        자료를 바탕으로 원티드랩에서 표현을 수정하고 이의 배열
+                        및 구성을 편집하여 완성한 원티드랩의 저작자산이자
+                        영업자산입니다. 본 정보 및 데이터베이스의 일부 내지는
+                        전부에 대하여 원티드랩의 동의 없이 무단전재 또는 재배포,
+                        재가공 및 크롤링할 수 없으며, 게재된 채용기업의 정보는
+                        구직자의 구직활동 이외의 용도로 사용될 수 없습니다.
+                        원티드랩은 <strong>블루비커</strong>에서 게재한 자료에
+                        대한 오류나 그 밖에 원티드랩이 가공하지 않은 정보의
+                        내용상 문제에 대하여 어떠한 보장도 하지 않으며, 사용자가
+                        이를 신뢰하여 취한 조치에 대해 책임을 지지
+                        않습니다.&nbsp;
+                        <strong>
+                          &lt;저작권자 (주)원티드랩. 무단전재-재배포금지&gt;
+                        </strong>
+                      </p>
+                    </div>
+                  )}
                 </div>
+
                 <div className="StealingWarning_StealingWarning_space__O_Fvk"></div>
               </section>
             </div>
