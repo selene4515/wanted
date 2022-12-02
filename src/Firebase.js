9415d0715713cae2c96e2eaa6154685a4c31cf2e
ming <44185091+selene4515@users.noreply.github.com>
2022-12-02 17:56:20 +0900

    Create Firebase.js

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAFAW-tT7Ppb5kCuOAt0-IXWeoyr4m_qFw",
+  authDomain: "wanted-3deb3.firebaseapp.com",
+  projectId: "wanted-3deb3",
+  storageBucket: "wanted-3deb3.appspot.com",
+  messagingSenderId: "718327969756",
+  appId: "1:718327969756:web:b340c27107dc8ee9580119",
+  measurementId: "G-P27VE3FKNJ",
+};
+
+export default initializeApp(firebaseConfig);
